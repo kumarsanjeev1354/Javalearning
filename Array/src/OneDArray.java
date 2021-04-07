87dc782f07aa7e82fd3cb696517d13fbea6e8de7
kumarsanjeev1354 <sksanjeev5073@gmail.com>
2021-04-07 13:39:01 +0530

    Commiting OneDArray

---
+++ Array/src/OneDArray.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class OneDArray {
+//@author sanjeev
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] arr1= {1,2,3};//used to create with specified elements
+		int[] arr;
+		arr=new int[5];//used to create an array if elements of array are known
+		
+	}
+
+}
