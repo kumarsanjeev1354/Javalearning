0661b6e814303943f7d3542365781c628fec3628
kumarsanjeev1354 <31682325+kumarsanjeev1354@users.noreply.github.com>
2023-01-06 11:39:30 +0530

    New changes added

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -3,23 +3,23 @@ public class Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//int[] arr;
-/*int n;
+int[] arr;
+int n;
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter size of array:");
 n=sc.nextInt();
-int[] arr=new int[n];
+int[] arr1=new int[n];
 System.out.println("Enter elements");
 for(int i=0;i<n;i++)
 {
-	arr[i]=sc.nextInt();
+	arr1[i]=sc.nextInt();
 }
 System.out.println("Elements of array are:");
 for(int i=0;i<n;i++)
 {
-System.out.println(arr[i]);
+System.out.println(arr1[i]);
 }
-//System.out.println(Size of array)*/
+System.out.println("Size of array" + " " + n);
 		
         System.out.println("Hello 1");
 		
